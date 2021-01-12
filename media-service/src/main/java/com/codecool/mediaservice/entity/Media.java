8ef29c323d1f8e799c7457281785db5b5961d4ef
pFuzzy <p.fazii888@gmail.com>
2021-01-12 10:58:51 +0100

    add Media entity

---
+++ media-service/src/main/java/com/codecool/mediaservice/entity/Media.java
@@ -0,0 +1,28 @@
+package com.codecool.mediaservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Media {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String url;
+}
