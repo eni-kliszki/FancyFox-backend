f6c3f949f26d1ecb501cb41396cabf4c9b16cfae
pFuzzy <p.fazii888@gmail.com>
2021-01-12 11:28:00 +0100

    add restTemplate

--- media-service/src/main/java/com/codecool/mediaservice/MediaServiceApplication.java
+++ media-service/src/main/java/com/codecool/mediaservice/MediaServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,6 +20,12 @@ public class MediaServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MediaServiceApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
     @Bean
