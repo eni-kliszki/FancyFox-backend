3f246885695b8271d9f4470fd1cb512f864607b6
pFuzzy <p.fazii888@gmail.com>
2021-01-12 13:05:50 +0100

    complete converting service data into one class

--- media-service/src/main/java/com/codecool/mediaservice/controller/MediaController.java
+++ media-service/src/main/java/com/codecool/mediaservice/controller/MediaController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,19 +21,34 @@ public class MediaController {
     @Autowired
     private MediaRepository mediaRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/all")
-    public ResponseEntity<List<Media>> getAllMedia(){
+    public ResponseEntity<List<Media>> getAllMedia() {
         return ResponseEntity.ok(mediaRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DetailedMedia> getMediaById(@PathVariable("id") Long id){
+    public ResponseEntity<DetailedMedia> getMediaById(@PathVariable("id") Long id) {
         Media mediaFromDb = mediaRepository.findMediaById(id);
+        String[] commentsFromService = restTemplate.getForEntity("http://localhost:8090/comment/" + id, String[].class).getBody();
+        List<String> comments = new ArrayList<>();
+        if(commentsFromService != null){
+            comments.addAll(Arrays.asList(commentsFromService));
+        }
+        Integer rating = restTemplate.getForEntity("http://localhost:8070/rating/" + id, Integer.class).getBody();
+
         DetailedMedia detailedMedia = DetailedMedia.builder()
                 .id(mediaFromDb.getId())
                 .title(mediaFromDb.getTitle())
                 .url(mediaFromDb.getUrl())
                 .build();
+        detailedMedia.setComments(comments);
+        if(rating != null){
+            detailedMedia.setRating(rating.longValue());
+        }
+
         return ResponseEntity.ok(detailedMedia);
     }
 }
